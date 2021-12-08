dfd86af888735d018b49240138361129d756a550
EngineerDogIta <bellayari@gmx.com>
2021-12-08 01:07:50 +0100

    Didnt manage to finish day 5, gonna do laider

--- day/5/Line.py
+++ day/5/Line.py
@@ -73,6 +73,7 @@ class Line:
         assert(type(p1) == Point and type(p2) == Point)
         self.p1 = p1
         self.p2 = p2
+        self.overlaps_with = None
 
     def create_line_by_coordinates(self, x1, y1, x2, y2):
         """
@@ -103,8 +104,10 @@ class Line:
         >>> l1 == l3
         False
         """
-        assert(type(__o) == Line)
-        return self.p1 == __o.p1 and self.p2 == __o.p2
+        if __o != None:
+            assert(type(__o) == Line)
+            return self.p1 == __o.p1 and self.p2 == __o.p2
+        return False
 
     def list_points(self):
         """
@@ -143,17 +146,57 @@ class Line:
         >>> l2 = Line(Point(1, 1), Point(1, 2))
         >>> l1.overlaps(l2)
         True
-        >>> l3 = Line(Point(1, 2), Point(3, 5))
+        >>> l3 = Line(Point(1, 0), Point(1, 2))
         >>> l1.overlaps(l3)
+        True
+        >>> l4 = Line(Point(1, 4), Point(1, 6))
+        >>> l1.overlaps(l4)
         False
+        >>> l5 = Line(Point(1, 0), Point(1, 4))
+        >>> l1.overlaps(l5)
+        True
         """
         assert(type(l) == Line)
-        if self.is_vertical() and l.is_vertical():
-            return self.p1.x >= l.p1.x and (self.p1.x == l.p1.x or self.p1.x == l.p2.x or self.p2.x == l.p1.x or self.p2.x == l.p2.x)
-        elif self.is_horizontal() and l.is_horizontal():
-            return self.p1.y >= l.p1.y and (self.p1.y == l.p1.y or self.p1.y == l.p2.y or self.p2.y == l.p1.y or self.p2.y == l.p2.y)
+        is_overlapping = False
+        if (self.is_vertical() and l.is_vertical() and self.p1.x == l.p1.x):
+            max_self_y = max(self.p1.y, self.p2.y)
+            min_self_y = min(self.p1.y, self.p2.y)
+            max_l_y = max(l.p1.y, l.p2.y)
+            min_l_y = min(l.p1.y, l.p2.y)
+            is_overlapping = max_self_y >= max_l_y or min_self_y >= min_l_y
+        elif(self.is_horizontal() and l.is_horizontal() and self.p1.y == l.p1.y):
+            max_self_x = max(self.p1.x, self.p2.x)
+            min_self_x = min(self.p1.x, self.p2.x)
+            max_l_x = max(l.p1.x, l.p2.x)
+            min_l_x = min(l.p1.x, l.p2.x)
+            is_overlapping = max_self_x >= max_l_x or min_self_x >= min_l_x
+        if self.overlaps_with == None:
+            self.overlaps_with = l
+        # if l.overlaps_with == None:
+        #     l.overlapse_with = self
+        return is_overlapping
+    
+    def number_of_overlaps(self, l : object) -> int:
+        """
+        Returns the number of points that overlaps the line l
+        >>> l1 = Line(Point(1, 1), Point(1, 3))
+        >>> l2 = Line(Point(1, 1), Point(1, 2))
+        >>> l1.number_of_overlaps(l2)
+        2
+        >>> l3 = Line(Point(1, 2), Point(3, 5))
+        >>> l1.number_of_overlaps(l3)
+        0
+        >>> l4 = Line(Point(1, 1), Point(1, 4))
+        >>> l1.number_of_overlaps(l4)
+        3
+        """
+        assert(type(l) == Line)
+        if (self.is_vertical() and l.is_vertical() and self.p1.x == l.p1.x):
+            return max(0, min(self.p2.y, l.p2.y) - max(self.p1.y, l.p1.y) + 1)
+        elif(self.is_horizontal() and l.is_horizontal() and self.p1.y == l.p1.y):
+            return max(0, min(self.p2.x, l.p2.x) - max(self.p1.x, l.p1.x) + 1)
         else:
-            return False
+            return 0
 
 
     def __repr__(self) -> str:
@@ -163,23 +206,23 @@ class Line:
         >>> repr(l)
         'Line((1, 2), (3, 4))'
         """
-        return f'Line({self.p1}, {self.p2})'
-    
-    def points_covered(self):
-        """
-        Return the list of points covered by the line
+        return f'Line({self.p1}, {self.p2})'    
+
+    def points(self):
+        """returns all the points covered by the line
         >>> l = Line(Point(1, 2), Point(1, 5))
-        >>> l.points_covered()
-        [(1, 2), (1, 3), (1, 4), (1, 5)]
-        >>> l = Line(Point(3, 2), Point(5, 2))
-        >>> l.points_covered()
-        [(3, 2), (4, 2), (5, 2)]
+        >>> l.points()
+        [Point(1, 2), Point(1, 3), Point(1, 4), Point(1, 5)]
         """
-        if self.is_vertical():
-            return [(self.p1.x, i) for i in range(self.p1.y, self.p2.y + 1)]
-        elif self.is_horizontal():
-            return [(i, self.p1.y) for i in range(self.p1.x, self.p2.x + 1)]
-        return []
+        return [Point(i, j) for i in range(self.p1.x, self.p2.x + 1) for j in range(self.p1.y, self.p2.y + 1)]
+
+    def length(self):
+        """returns the length of the line
+        >>> l = Line(Point(1, 2), Point(1, 5))
+        >>> l.length()
+        3
+        """
+        return abs(self.p1.x - self.p2.x) + abs(self.p1.y - self.p2.y)
 
 if __name__ == "__main__":
     import doctest
--- day/5/main_1.py
+++ day/5/main_1.py
@@ -1,80 +1,30 @@
 import os
-# from Line import Line, Point
+from Line import Line, Point
 
 if __name__ == "__main__":
     with open(os.path.join(os.path.dirname(__file__), 'input.txt'), 'r', encoding='UTF-8') as f:
         data = f.read().splitlines()
 
-    # lines = []
-    # for line in data:
-    #     points = line.split(' -> ')
-    #     p1 = points[0].split(',')
-    #     p2 = points[1].split(',')
-    #     p1 = Point(int(p1[0]), int(p1[1]))
-    #     p2 = Point(int(p2[0]), int(p2[1]))
-    #     line_points = Line(p1, p2)
-    #     if line_points.is_vertical() or line_points.is_horizontal():
-    #         lines.append(line_points)
-    grid = {(0, 0): 0}
+    overlapping_lines = 0
     all_lines = []
     for line in data:
-        points = line.split(' -> ')
-        p1 = points[0].split(',')
-        p2 = points[1].split(',')
-        p1 = (int(p1[0]), int(p1[1]))
-        p2 = (int(p2[0]), int(p2[1]))
-        line_points = (p1, p2)
-        if p1[0] == p2[0]:
-            all_lines.append(line_points)
-            from_y, to_y = min(p1[1], p2[1]), max(p1[1], p2[1])
-            if from_x != to_x:
-                for ys in range(from_x, to_x):
-                    if (ys, p1[0]) in grid.keys():
-                        grid[(ys, p1[0])] += 1
-                    else:
-                        grid[(ys, p1[0])] = 1
-            else:
-                grid[p1] += 1
-        elif p1[1] == p2[1]:
-            all_lines.append(line_points)
-            from_x, to_x = min(p1[0], p2[0]), max(p1[0], p2[0])
-            if from_x != to_x:
-                for xs in range(from_x, to_x):
-                    if (p1[1], xs) in grid.keys():
-                        grid[(p1[1], xs)] += 1
-                    else:
-                        grid[(p1[1], xs)] = 1
-            else:
-                grid[p1] += 1
+        line_points = line.split(' -> ')
+        p1 = line_points[0].split(',')
+        p2 = line_points[1].split(',')
+        p1 = Point(int(p1[0]), int(p1[1]))
+        p2 = Point(int(p2[0]), int(p2[1]))
+        l1 = Line(p1, p2)
+        if l1.is_horizontal() or l1.is_vertical():
+            all_lines.append(l1)
 
-    overlapping_lines = 0
-    for point in grid.keys():
-        if grid[point] == 2:
-            overlapping_lines += 1
+    for i in range(len(all_lines)):
+        for j in [k for k in range(len(all_lines)) if k != i]:
+            if all_lines[i].overlaps(all_lines[j]):
+                overlapping_lines += 1
+                break
+
+
     print(f'overlapping_lines: {overlapping_lines}')
-
-    # represent the grid
-    min_x = min(grid.keys(), key=lambda x: x[0])[0]
-    max_x = max(grid.keys(), key=lambda x: x[0])[0]
-    min_y = min(grid.keys(), key=lambda x: x[1])[1]
-    max_y = max(grid.keys(), key=lambda x: x[1])[1]
-
-    for x in range(min_x, max_x + 1):
-        for ys in range(min_y, max_y + 1):
-            if (ys, x) in grid.keys():
-                print(grid[(ys, x)], end='')
-            else:
-                print('.', end='')
-        print()
-
-    # overlapping_lines = 0
+    print(f'number of lines that they overleap at least one time {len([x for x in all_lines if x.overlaps_with != None])}')
+    print(f'overlapping_points {sum([x.number_of_overlaps(x.overlaps_with) for x in all_lines if x.overlaps_with != None])}')
     
-    # for i1 in range(len(lines)):
-    #     for i2 in range(len(lines)):
-    #         if i1 != i2:
-    #             line1 = lines[i1]
-    #             line2 = lines[i2]
-    #             if line1.overlaps(line2):
-    #                 overlapping_lines += 1
-
-    # print(f'Overlapping lines: {overlapping_lines}')
