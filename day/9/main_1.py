b14a06c409111f972b32c2689e37b0bf48b6f2a6
EngineerDogIta <bellayari@gmx.com>
2021-12-11 18:26:27 +0100

    Added .todo s

---
+++ day/9/main_1.py
@@ -0,0 +1,7 @@
+import os
+
+if __name__ == '__main__':
+    with open(os.path.join(os.path.dirname(__file__), 'input.txt'), 'r', encoding='UTF-8') as f:
+        data = f.read().splitlines()
+
+    # TODO
