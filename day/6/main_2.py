ce0025cfdcc0f87fff679db75118b2ef74599ac6
EngineerDogIta <bellayari@gmx.com>
2021-12-11 17:58:53 +0100

    Day 6 part twooo BABABoey

--- day/6/main_2.py
+++ day/6/main_2.py
@@ -1,19 +1,34 @@
 import os
-from Lanternfish import Lanternfish
+from collections import Counter
 
 if __name__ == '__main__':
     with open(os.path.join(os.path.dirname(__file__), 'input.txt'), 'r', encoding='UTF-8') as f:
         data = f.read().splitlines()
 
-    fishes = [Lanternfish(int(x)) for x in data[0].split(',')]
+    fishes = [int(x) for x in data[0].split(',')]
+    # fishes = [3,4,3,1,2]
+    fishes_counter = {x: 0 for x in range(9)}
+    fishes_counter.update(dict(Counter(fishes)))
 
-    for day in range(256):
-        lanternfish_2add = []
-        for i_fish in range(len(fishes)):
-            new_fish = fishes[i_fish].pass_a_day()
-            if new_fish != None:
-                lanternfish_2add.append(new_fish)
-        fishes.extend(lanternfish_2add)
-        print(f'Day {day} passed {len(fishes)} fishes')
+    # print(','.join([str(x) for x in range(9)]))
+    # print(','.join([str(fishes_counter[x]) for x in range(9)]))
 
-    print(f'Number of fishes after 80 days {len(fishes)}')
+    # days = 84
+
+    days = 256
+
+    for i in range(days):
+        # print(f'Day {i} len {fishes_counter}')
+        zero_counter = fishes_counter[0]
+
+        new_fishes_counter = {x: 0 for x in range(9)}
+
+        for counter in range(1,9):
+            new_fishes_counter[counter-1] = fishes_counter[counter]
+        new_fishes_counter[8] = zero_counter
+        new_fishes_counter[6] += zero_counter
+
+        fishes_counter = new_fishes_counter
+        # print(','.join([str(fishes_counter[x]) for x in range(9)]))
+    
+    print(f'Number of fishes after {days} days: {sum(fishes_counter.values())}')
